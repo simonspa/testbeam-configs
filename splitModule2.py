9500cda47f60ff78941de64bf30d6080676631a9
Simon Spannagel <simon.spannagel@desy.de>
2016-04-25 16:11:40 +0200

    dump new stuff that people randomly left on the DAQ computer... :-/

--- splitModule2.py
+++ splitModule2.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
+#python splitModule2.py -i D4028/dacParameters_D4028_ia25-trim40-2016-04.dat -g 19
+#python splitModule2.py -i D4028/trimParameters_D4028_ia25-trim40-2016-04.dat -g 4160
 
 import sys, getopt
 
 def main(argv):
     filename = ''
-    granularity = 0
+    lines_per_file = 0
 
     try:
-        opts, args = getopt.getopt(argv,"hi:g:",["ifile=","granularity="])
+        opts, args = getopt.getopt(argv,"hi:g:",["ifile=","lines_per_file="])
     except getopt.GetoptError:
-        print 'test.py -i <inputfile> -g <granularity>'
+        print 'test.py -i <inputfile> -g <lines_per_file>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -17,8 +19,8 @@ def main(argv):
             sys.exit()
         elif opt in ("-i", "--ifile"):
             filename = arg
-        elif opt in ("-g", "--granularity"):
-            granularity = int(arg)
+        elif opt in ("-g", "--lines_per_file"):
+            lines_per_file = int(arg)
 
     with open(filename,'r') as f:
         lines = f.readlines()
@@ -26,7 +28,7 @@ def main(argv):
     for o in range(len(lines)):
         if lines!=[]: 
             with open(f.name.split("_")[0] +"_C" + str(o) + '.dat','w') as NewFile:
-                for i in range(granularity):
+                for i in range(lines_per_file):
                     if lines!=[]:NewFile.write(lines.pop(0))
                 
 
